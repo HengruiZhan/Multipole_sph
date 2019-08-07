a66bd4ddb995ac348bafcdb15c133c6f6afde648
HengruiZhan <48325976+HengruiZhan@users.noreply.github.com>
2019-08-07 02:22:30 -0400

    Create L2_norm_error.py

---
+++ phi/sphre_phi/r=0.3/nr = 256_nz = 512/off_o.5dr/L2_norm_error/L2_norm_error.py
@@ -0,0 +1,28 @@
+# L2 norm error of sphere perfect
+# r = 0.3
+# the number of grid point nr= 256, nz= 512
+# rlim = (0.0, 1.0)
+# zlim = (-1.0, 1.0)
+# evaluate at the grid cell center face with r_grid_face = r_grid_center - dr/2
+lmax = 0
+4.874349439459526e-14
+lmax = 2
+4.8677542990295295e-14
+lmax = 4
+4.868736732584123e-14
+lmax = 6
+4.867531791015034e-14
+lmax = 8
+4.8685852961858525e-14
+lmax = 10
+4.868331506086169e-14
+lmax = 12
+4.868620948953999e-14
+lmax = 14
+4.868752450138756e-14
+lmax = 16
+4.869590845348828e-14
+lmax = 18
+4.873377469774911e-14
+lmax = 20
+4.87906241542516e-14
